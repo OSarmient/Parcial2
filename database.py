72fa24a20f07d5caf1296f6b47d154352487d5ad
ctrlprogrammer <sebastianto1999@gmail.com>
2020-05-15 20:13:22 -0500

    V1.1 vehicles and create get asc data in database

--- database.py
+++ database.py
@@ -18,7 +18,7 @@ def create_database(database_name):
         return False
 
 
-def get_database(database_name, mode):
+def get_database(database_name, mode="r"):
     isFile = os.path.isfile(databases_path + database_name + ".txt")
     if isFile == False:
         if mode == "dev":
@@ -75,11 +75,11 @@ def get_by_uid(database_name, uid):
     return False
 
 
-def get_by_property(database_name, property, value):
+def get_by_property(database_name, property="uid", value=""):
     database = get_database(database_name, "r")
     if database != False:
         all_data = database.read().split("\n")
-        for i in range(len(all_data) - 3):
+        for i in range(len(all_data) - 1):
             if ast.literal_eval(all_data[i])[property] == value:
                 return ast.literal_eval(all_data[i])
     else:
@@ -88,21 +88,37 @@ def get_by_property(database_name, property, value):
         return False
     return False
 
-# ####Examples
 
-# ##Create database
-# create_database("customers")
+def get_multi_database_data(database_name, property="uid", value=""):
+    database = get_database(database_name, "r")
+    return_data = []
+    if database != False:
+        all_data = database.read().split("\n")
+        for i in range(len(all_data) - 1):
+            if ast.literal_eval(all_data[i])[property] == value:
+                return_data.append(ast.literal_eval(all_data[i]))
+        return return_data
+    else:
+        if mode == "dev":
+            print("la base de datos no existe")
+        return False
+    return False
 
-# ##Include customers in customers database
-# save_in_database("customers", {"id": "1245125", "name": "Sebastian", "lastname": "Torres"})
-# save_in_database("customers", {"id": "1251", "name": "Nicolas", "lastname": "Ochoa"})
-# save_in_database("customers", {"id": "23546", "name": "Margarita", "lastname": "Romero"})
-# save_in_database("customers", {"id": "12512512", "name": "Luz", "lastname": "Beltran"})
+    # ####Examples
 
-# ##Get data by uid
-# print(get_by_uid("customers", 2))
+    # ##Create database
+    # create_database("customers")
 
-# ##Get data with other properties
-# print(get_by_property("customers", "name", "Luz"))
-# print(get_by_property("customers", "id", "1251"))
-# print(get_by_property("customers", "uid", 2))
+    # ##Include customers in customers database
+    # save_in_database("customers", {"id": "1245125", "name": "Sebastian", "lastname": "Torres"})
+    # save_in_database("customers", {"id": "1251", "name": "Nicolas", "lastname": "Ochoa"})
+    # save_in_database("customers", {"id": "23546", "name": "Margarita", "lastname": "Romero"})
+    # save_in_database("customers", {"id": "12512512", "name": "Luz", "lastname": "Beltran"})
+
+    # ##Get data by uid
+    # print(get_by_uid("customers", 2))
+
+    # ##Get data with other properties
+    # print(get_by_property("customers", "name", "Luz"))
+    # print(get_by_property("customers", "id", "1251"))
+    # print(get_by_property("customers", "uid", 2))
--- vehicles.py
+++ vehicles.py
@@ -5,7 +5,7 @@ import database
 # pasajeros), tipo de combustible, capacidad de pasajeros, capacidad de carga, número de chasis y
 # número de motor.
 
-#mode = "dev"
+# mode = "dev"
 database_name = "vehicles"
 props = [
     {
@@ -98,12 +98,11 @@ def insert():
                 data[i["data"]] = i["value"]
                 bucle = False
 
-    print("Guardado en la base de datos")
     database.save_in_database(database_name, data)
+    print("Guardado en la base de datos.")
 
 
 def get_all():
-    print()
     data = database.get_data_in_database(database_name)
     for i in data:
         keys = i.keys()
@@ -111,7 +110,37 @@ def get_all():
         for j in keys:
             print(str(j) + ": " + str(i[j]))
         print("----------------")
-        print()
+
+
+def get_customer_vehicles():
+    bucle = True
+    while bucle:
+
+        print("Digite s en cualquier momento para salir.")
+        customer_id = str(
+            input("Digite el numero de identificación del cliente: "))
+
+        if customer_id.lower() != "s":
+            customer_vehicles = database.get_multi_database_data(
+                database_name, "cliente", customer_id)
+
+            if type(customer_vehicles) == list and len(customer_vehicles) > 0:
+                print()
+                print("Existen " + str(len(customer_vehicles)) +
+                      " vehiculos asociados a este numero de indentificación.")
+                for i in customer_vehicles:
+                    keys = i.keys()
+                    print("----------------")
+                    for j in keys:
+                        print(str(j) + ": " + str(i[j]))
+                    print("----------------")
+                print()
+            else:
+                print()
+                print("No existen vehiculos asociados a este numero de identificación.")
+                print()
+        else:
+            bucle = False
 
 
 def show_menu():
@@ -120,7 +149,7 @@ def show_menu():
     print()
     print("1. Agregar un carro [1]")
     print("2. Ver todos los carros [2]")
-    print("3. Buscar carro por su placa [3]")
+    print("3. Buscar carros de un cliente [3]")
     print("4. Agregar un carro [4]")
     print("5. Agregar un carro [5]")
     print("5. Mostrar menú [5]")
@@ -138,14 +167,18 @@ def start():
         try:
             print("5. Mostrar menú [5]")
             option = int(input("Digita la opción que desees ejecutar: "))
+            print()
             if option == 1:
                 insert()
             elif option == 2:
                 get_all()
+            elif option == 3:
+                get_customer_vehicles()
             elif option == 5:
                 show_menu()
             elif option == 6:
                 bucle = False
+            print()
         except NameError:
             print(NameError)
             print("5. Mostrar menú [5]")
