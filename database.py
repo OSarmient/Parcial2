7b4ea815c47432924f37c11ce21dd8c3828c4063
fmarquezf <65417004+fmarquezf@users.noreply.github.com>
2020-05-18 11:11:04 -0500

    Update database.py

--- database.py
+++ database.py
@@ -89,7 +89,7 @@ def get_by_property(database_name, property="uid", value=""):
     if database != False:
         all_data = database.read().split("\n")
         for i in range(len(all_data) - 1):
-            if ast.literal_eval(all_data[i])[property].strip() == value:
+            if ast.literal_eval(all_data[i])[property] == value:
                 return ast.literal_eval(all_data[i])
     else:
         if mode == "dev":
@@ -104,7 +104,7 @@ def get_multi_database_data(database_name, property="uid", value=""):
     if database != False:
         all_data = database.read().split("\n")
         for i in range(len(all_data) - 1):
-            if ast.literal_eval(all_data[i])[property].strip() == value:
+            if ast.literal_eval(all_data[i])[property] == value:
                 return_data.append(ast.literal_eval(all_data[i]))
         return return_data
     else:
@@ -114,32 +114,33 @@ def get_multi_database_data(database_name, property="uid", value=""):
     return False
 
 
-def delete_data_by_property(database_name, property="uid", value=" "):
-    data_in_database = get_data_in_database(database_name)
-    row = get_by_property(database_name, property, value)
-    if row != False:
-        data_in_database.pop(row["uid"])
-        write_database = get_database(database_name, "w")
-        write_database.write("")
-        for i in data_in_database:
-            save_in_database(database_name, i)
-    else:
-        print("No existe en la base de datos.")
-
-
 def delete_data_by_uid(database_name, property="uid", value=" "):
     data_in_database = get_data_in_database(database_name)
     vehicle = get_by_property(database_name, property, value)
-    if vehicle != False:
-        data_in_database.pop(vehicle["uid"])
-        write_database = get_database(database_name, "w")
-        write_database.write("")
-        for i in data_in_database:
-            save_in_database(database_name, i)
-    else:
-        print("No existe en la base de datos.")
+    data_in_database.pop(vehicle["uid"])
+    write_database = get_database(database_name, "w")
+    write_database.write("")
+    for i in data_in_database:
+        save_in_database(database_name, i)
 
+def delete_data_by_service_code(database_name, property="SC", value = ""):
+    data_in_database = get_data_in_database(database_name)
+    service = get_by_property(database_name, property, value)
+    data_in_database.pop(service["SC"])
+    write_database = get_database(database_name, "w")
+    write_database.write("")
+    for i in data_in_database:
+        save_in_database(database_name, i)
 
+def delete_data_by_noid(database_name, property="uid", value=" "):
+    data_in_database = get_data_in_database(database_name)
+    client3 = get_by_property(database_name, property, value)
+    data_in_database.pop(client3["uid"])
+    write_database = get_database(database_name, "w")
+    write_database.write("")
+    for i in data_in_database:
+        save_in_database(database_name, i)
+        
     # ####Examples
 
     # ##Create database
@@ -158,4 +159,3 @@ def delete_data_by_uid(database_name, property="uid", value=" "):
     # print(get_by_property("customers", "name", "Luz"))
     # print(get_by_property("customers", "id", "1251"))
     # print(get_by_property("customers", "uid", 2))
-
