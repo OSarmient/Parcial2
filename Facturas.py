ff9a1df6b6ab8e9c7e58134ae4227c8781464a78
fmarquezf <65417004+fmarquezf@users.noreply.github.com>
2020-05-18 16:08:13 -0500

    Update Facturas.py

--- Facturas.py
+++ Facturas.py
@@ -3,11 +3,10 @@ import database
 database_name4 = "service_asked"
 database_name5 = "Facturas"
 
-def hola():
+def get_bills(iden):
     price = 0                                               #Precio es 0
     BD = 0
     bill = database.get_data_in_database(database_name4)    #llama un archivo
-    iden = input("Digite su número de identificación: ")    #Mete el id
     for i in bill:                                          #Pasa por cada uno de los elementos del archivo
         for j in i:                                         #Pasa por los items de los elementos del archivo
             if j == ("noid1: " + iden):                     #Si el item es igual que el ID
@@ -19,14 +18,11 @@ def hola():
                 i.append(price)
                 BD = database.count_database(database_name5)
                 i.insert(0, ("Factura No. " + str(BD)))
-                print(i)
                 database.save_in_database2(database_name5, i)
                 price = 0
                 break
-            else:
-                print("No hay facturas registradas para este cliente")
-                break
         break
+    
 def get_allF():
     print()
     data = database.get_data_in_database(database_name5)
@@ -34,6 +30,24 @@ def get_allF():
         print("-----------------")
         print (i)
         print("-----------------")
+
+def hola():
+    iden = input("Digite su número de identificación: ")
+    bill1 = database.get_data_in_database(database_name5)
+    loop = True
+    for i in bill1:
+        while loop:
+            for j in i:
+                 if j == ("noid1: " + iden):
+                    print(i)
+                    loop = False
+                    break
+                 else:
+                    loop = True
+            break
+        break
+    if loop == True:
+        print("No exite una factura asociada a este cliente")
 
 #def fin_servicio(archivo):
 #    data = {}
