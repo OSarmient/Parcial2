2fe9c61071217a9a6dca9121fc18c84a276366f6
OSarmient <49238469+OSarmient@users.noreply.github.com>
2020-06-07 18:13:42 -0500

    Add files via upload

---
+++ parcialV2/service.py
@@ -0,0 +1,48 @@
+from core.module import ModuleBase
+
+class Service(ModuleBase):
+    def __init__(self):
+        self.database_name = "service"
+        self.singularity = "servicio"
+        self.singular = "El servicio"
+        self.flush_singular = "un servicio"
+        self.menu_options = []
+        self.main_field = "placa"
+        self.properties = [
+            {
+                "data": "numero",
+                "display": "Escriba el codigo que el servicio que deaea: ",
+                "value": "",
+                "type": "int",
+                "ajust": "6"
+            },
+
+            {
+                "data": "nombre",
+                "display": "Escriba el nombre de el servicio que desea: ",
+                "value": "",
+                "type": "str",
+                "ajust": "25"
+            },
+
+            {
+                "data": "precio",
+                "display": "Escriba cuanto cuaesta el servicio por hora: ",
+                "value": "",
+                "type": "int",
+                "ajust": "7"
+            },
+
+            {
+                "data": "horas",
+                "display": "Escriba por cuantas horas quire contratar el servicio: ",
+                "value": "",
+                "type": "int",
+                "ajust": "2"
+            }
+        ]
+
+        super().__init__()
+    
+service = Service()
+service.start()
