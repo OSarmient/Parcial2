e7cb36e523f3fbf6a62008bf4e7c4dd7d15de1f3
fmarquezf <65417004+fmarquezf@users.noreply.github.com>
2020-06-08 18:11:59 -0500

    Update main.py

--- parcialV2/main.py
+++ parcialV2/main.py
@@ -1,44 +1,15 @@
-import vehicles
-import service
-import clients
+from vehicles import Vehicles
+from service import Service
+from clients import Clients
 from bills import Facturas
+from core.module import ModuleBase
 
 
 class Main():
-    def __init__(self):
-        bucle = True    
-        menu()
-        while bucle:
-            try:
-                option = int(input("Digita la opción que desees ejecutar: "))
-                print()
-                if option == 1:
-                    clients.start()
-                    menu()
-                elif option == 2:
-                    vehicles.start()
-                    menu()
-                elif option == 3:
-                    service.start()
-                    menu()
-                elif option == 4:
-                    facturas.get_allF()
-                    menu()
-                elif option == 5:
-                    facturas.hola()
-                    menu()
-                elif option == 6:
-                    menu()
-                elif option == 7:
-                    bucle = False
-
-                print()
-            except NameError:
-                print(NameError)
-                print("7. Mostrar menú de modulos [7]")
-                print("La opción digitada es invalida (debe ser un número en el menú).")
-                print()
-    
+    def __init__(self):    
+        self.menu()
+        self.mainstart()
+        
     def menu(self):
         print("---Menu de modulos----: ")
         print("1. Clientes. [1]")
@@ -48,9 +19,37 @@ class Main():
         print("5. Buscar Facturas de un cliente [5]")
         print("6. Mostar menu. [6]")
         print("7. Salir [7]")
-    
-    inicio = Main()
-    inicio.start()
 
+    def mainstart(self):
+        bucle = True
+        while bucle:
+            #try:
+            option = int(input("Digita la opción que desees ejecutar: "))
+            print()
+            if option == 1:
+                Clients().start()
+                self.menu()
+            elif option == 2:
+                Vehicles().start()
+                self.menu()
+            elif option == 3:
+                Service().start()
+                self.menu()
+            elif option == 4:
+                Facturas().get_all()
+                self.menu()
+            elif option == 5:
+                Facturas().hola()
+                self.menu()
+            elif option == 6:
+                self.menu()
+            elif option == 7:
+                bucle = False
 
+                print()
+          #  except NameError:
+           #     print(NameError)
+            #    print("7. Mostrar menú de modulos [7]")
+             #   print("La opción digitada es invalida (debe ser un número en el menú).")
+              #  print()
         
